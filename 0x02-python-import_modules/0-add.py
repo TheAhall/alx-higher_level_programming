719af94fa167b17caf23f9d0d2c6717cd9110a7e
HALLAL <hmdhallal@gmail.com>
2024-01-04 06:03:51 -0800

    [200~0x02-python-import_modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
-sum_string = "{} + {} = {}".format(a, b, add(a, b))
-print(sum_string)
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    sum_string = "{} + {} = {}".format(a, b, add(a, b))
+    print(sum_string)
