53abf388f3f3ca99eac3408b12d1a61420bfbc58
HALLAL <hmdhallal@gmail.com>
2024-01-22 06:28:20 -0800

    0x05-python-exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    indexsum = 0 
+    for i in range(x):
+        try:
+            print(f"{my_list[i]}", end="")
+        except IndexError:
+            break
+        indexsum += 1
+    print()
+    return indexsum
