d97ec5ad5f809a9e0d213b679b8cf6ec96831b3b
HALLAL <hmdhallal@gmail.com>
2024-01-30 06:20:04 -0800

    0x09-python-everything_is_object

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+Define function returns a copy of a list
+"""
+
+
 def copy_list(l):
+    """Return a copy of a list"""
     copied_list = []
     copied_list = list(l)
     return copied_list
