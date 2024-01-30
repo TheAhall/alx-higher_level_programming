cab23b899cc37145ac1c47411c25062b2c155b64
HALLAL <hmdhallal@gmail.com>
2024-01-30 06:47:19 -0800

    0x09-python-everything_is_object

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(num = [0]):
-    num[0] = num[0] + 1
-    return "BestSchool" + (", BestSchool" * (num[0] - 1))
+def magic_string(times = [0]):
+    times[0] = times[0] + 1
+    return "BestSchool" + (", BestSchool" * (times[0] - 1))
