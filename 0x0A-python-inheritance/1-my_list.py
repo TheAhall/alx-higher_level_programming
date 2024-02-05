c7f2c0388d67d1008ab43d81d3a2ac86c4d622cd
HALLAL <hmdhallal@gmail.com>
2024-02-05 05:12:18 -0800

    0x0A-python-inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#1/usr/bin/python3
+'''The Class MyList'''
+
+
+class MyList(list):
+    '''creating the class MyList'''
+    def print_sorted(self):
+        '''printing the sorted list'''
+        sorted_list = sorted(self)
+        print(sorted_list)
