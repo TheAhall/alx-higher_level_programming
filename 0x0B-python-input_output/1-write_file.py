2f706aeab24e30aa82e8ba7d6645bbd64b5b90d8
HALLAL <hmdhallal@gmail.com>
2024-02-06 07:23:36 -0800

    0x0B-python-input_output

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''Define write_file modul'''
+
+
+def write_file(filename="", text=""):
+    '''write_file: function that writes a string to a text file'''
+    with open(filename, mode='w', encoding='utf-8') as myfile:
+        myfile.read
