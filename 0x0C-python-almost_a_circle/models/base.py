b89e442a9067e5a2ceb4d2c755c1cb8eecb0a3cf
HALLAL <hmdhallal@gmail.com>
2024-02-12 13:09:49 -0800

    0x0C-python-almost_a_circle

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines Base class"""
+import json
+import csv
+import turtle
+
+
+class Base:
+    """Base class body"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize Id in a constructor"""
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
